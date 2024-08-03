3328ae57d4d05fa9e3f6a5d6baf51473c7572735
JSQ <j996730508@163.com>
2024-08-03 13:42:25 +0800

    types: 增加 postcss.config jsdoc 类型提示

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,9 @@
+/**
+ * @typedef {import('postcss').ProcessOptions} ProcessOptions
+ * @typedef {import('postcss').Plugin} Plugin
+ *
+ * @type {string | (ProcessOptions & {plugins?: Plugin[]})}
+ */
 export default {
   plugins: {
     tailwindcss: {},
