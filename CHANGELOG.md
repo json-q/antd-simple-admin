efa3d1a191358a967292909055cf090946e10635
JSQ <j996730508@163.com>
2024-08-05 21:55:49 +0800

    docs: add CHANGELOG, ant css remove :where selector

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# 0.0.3
+
+- 新增暗/亮色主题切换，并添加切换过渡动画
+- global css 添加 layer 降级，便于样式覆盖
+
+# 0.0.2
+
+- 移除所有 less 及其相关依赖
+
+# 0.0.1
+
+- react-router 路由渲染方式使用 `RouterProvider` 重构
+- router TS 类型定义优化，对 `IRouteObject` 进行类型扩展
+- 面包屑渲染逻辑优化
+- Menu 渲染逻辑优化
--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,18 @@ import dayjs from "dayjs";
 import "dayjs/locale/zh-cn";
 import App from "./App.tsx";
 import "@/styles/index.css";
+import { StyleProvider } from "antd-style";
 
 dayjs.locale("zh-cn");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
     <ConfigProvider locale={zhCN} theme={{ cssVar: true, hashed: false }}>
-      <AntApp>
-        <App />
-      </AntApp>
+      <StyleProvider hashPriority="high">
+        <AntApp>
+          <App />
+        </AntApp>
+      </StyleProvider>
     </ConfigProvider>
   </HelmetProvider>,
 );
