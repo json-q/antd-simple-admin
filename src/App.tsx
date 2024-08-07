1986d01b46cfb0ea7c1d61ddc6320052944100d5
JSQ <j996730508@163.com>
2024-08-07 20:39:00 +0800

    fix: static methods cannot consume Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Result } from "antd";
 import { ErrorBoundary } from "react-error-boundary";
-import ThemeControlProvider from "@/themes/ThemeControlProvider";
 import useStaticApp from "@/hooks/useStaticApp";
 import RenderRoutes from "@/routes";
 
@@ -8,17 +7,13 @@ const App: React.FC = () => {
   useStaticApp();
 
   return (
-    <ThemeControlProvider>
-      <ErrorBoundary
-        fallbackRender={(props) => {
-          return (
-            <Result status="error" title="出现了一些未知错误" subTitle={props.error.message} />
-          );
-        }}
-      >
-        <RenderRoutes />
-      </ErrorBoundary>
-    </ThemeControlProvider>
+    <ErrorBoundary
+      fallbackRender={(props) => {
+        return <Result status="error" title="出现了一些未知错误" subTitle={props.error.message} />;
+      }}
+    >
+      <RenderRoutes />
+    </ErrorBoundary>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -8,19 +8,22 @@ import dayjs from "dayjs";
 import "dayjs/locale/zh-cn";
 import App from "./App.tsx";
 import "@/styles/index.css";
+import ThemeControlProvider from "./themes/ThemeControlProvider.tsx";
 
 dayjs.locale("zh-cn");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
-      <ConfigProvider locale={zhCN} theme={{ cssVar: true, hashed: false }}>
-        <StyleProvider hashPriority="high">
-          <AntApp>
-            <App />
-          </AntApp>
-        </StyleProvider>
-      </ConfigProvider>
+      <ThemeControlProvider>
+        <ConfigProvider locale={zhCN} theme={{ cssVar: true, hashed: false }}>
+          <StyleProvider hashPriority="high">
+            <AntApp>
+              <App />
+            </AntApp>
+          </StyleProvider>
+        </ConfigProvider>
+      </ThemeControlProvider>
     </HelmetProvider>
   </StrictMode>,
 );
